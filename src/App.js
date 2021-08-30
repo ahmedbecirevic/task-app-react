e60f55e1489887b447b3de54b56e945b22bd946a
ahmedbecirevic <ahmed.becirevic@stu.ibu.edu.ba>
2021-08-31 00:33:57 +0200

    add mock server

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
 import Header from "./components/Header";
 import Tasks from './components/Tasks';
 import AddTask from './components/AddTask';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-const TASKS_ARRAY = [
-  {
-    id: 1,
-    text: 'Doctors Appointment',
-    day: 'Feb 5th at 2:30pm',
-    reminder: true,
-  },
-  {
-    id: 2,
-    text: 'Meeting at School',
-    day: 'Feb 6th at 1:30pm',
-    reminder: true,
-  },
-  {
-    id: 3,
-    text: 'Vet  Appointment',
-    day: 'Feb 8th at 6:30pm',
-    reminder: false,
-  },
-];
+const TASKS_ARRAY = [];
 
 export const ShowFormContext = React.createContext({
   showAddForm: () => {},
@@ -33,19 +15,44 @@ const App = () => {
   const [tasks, setTasks] = useState(TASKS_ARRAY);
   const [showForm, setShowForm] = useState(false);
 
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks();
+      setTasks(tasksFromServer);
+    };
+    getTasks();
+  }, [tasks]);
+
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks');
+    const data = await res.json();
+
+    return data;
+  };
+
   const showAddFormHandler = () => {
     setShowForm(prevValue => !prevValue);
   };
 
   const addTaskHandler = task => {
-    const id = Math.floor(Math.random() * 10000) + 1;
-    const newTask = { id, ...task };
-    setTasks(prevTasks => {
-      return [newTask, ...prevTasks];
-    });
+    axios
+      .post('http://localhost:5000/tasks', {
+        text: task.text,
+        day: task.day,
+        reminder: task.reminder,
+      })
+      .then(res => {
+        setTasks(prevTasks => [...prevTasks, res.data]);
+      });
+    // const id = Math.floor(Math.random() * 10000) + 1;
+    // const newTask = { id, ...task };
+    // setTasks(prevTasks => {
+    //   return [newTask, ...prevTasks];
+    // });
   };
 
-  const deleteTaskHandler = id => {
+  const deleteTaskHandler = async id => {
+    await fetch(`http://localhost:5000/tasks/${id}`, { method: 'DELETE' });
     setTasks(prevTasks => {
       const tempTasks = prevTasks.filter(task => task.id !== id);
       return tempTasks;
